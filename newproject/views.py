06ea72c4fcc40768d2929831416d7d090d01c7d1
Shalaka Sinha <Shalaka@Shalaka-MacBook-Air.local>
2016-01-26 10:21:29 +0530

    asynchronous comment feature added

--- newproject/views.py
+++ newproject/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from blog.models import Admin 
 from blog.models import Data
 from blog.models import likes
+from blog.models import comment
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AnonymousUser
 from django.shortcuts import get_object_or_404
@@ -25,22 +26,36 @@ post_title= []
 def home(request):
     return render(request, 'file.html')
 
-def reachhome(request):
- 	return render(request,'newfile.html')
+
 
 def check1(request):
 	uname = request.GET.get('username')
 	passwd = request.GET.get('password')
-	user = authenticate(username=uname, password=passwd)
-	login(request , user)
-	data = Data.objects.all()
-	if uname == None and passwd == None:
-		return render(request,'newfile.html',{ 'data':data })
-	if user:
-		return render(request,'newfile.html',{ 'data':data })
+	if (uname and passwd) or (uname==None and passwd == None):
+		user = authenticate(username=uname, password=passwd)
+		login(request , user)
+		data = Data.objects.all()
+		if uname == None and passwd == None:
+			return render(request,'newfile.html',{ 'data':data , 'x':"Logout" })
+		if user:
+			return render(request,'newfile.html',{ 'data':data ,'x':"Logout" })
+		else:
+			response="Incorrect Credentials"
+		return HttpResponse(response)
 	else:
-		response="Incorrect Credentials"
-	return HttpResponse(response)
+		response = "enter details"
+		return HttpResponse(response)
+
+
+
+def log(request):
+	if request.user.is_authenticated():
+		logout(request)
+		data = Data.objects.all()
+		return render(request,'newfile.html',{'data':data , 'x':"Login"})
+	else:
+		return render(request,'file.html')
+
 
 def register(request):
 	return render(request,'register.html')
@@ -51,16 +66,21 @@ def post(request, slug):
 	post_title = post
 	if request.user.is_authenticated():
 		l = likes.objects.filter(post = post, user = request.user)
+		c = comment.objects.filter(post = post)
+		
 		if not l:
-			return render(request,'post.html',{'posts':post , 'x':"Like" , 'usr':request.user})
+			return render(request,'post.html',{'posts':post , 'x':"Like" , 'usr':request.user , 'y':"Logout" , 'comment':c })
 		else :
-			return render(request,'post.html',{'posts':post , 'x':"Unlike" , 'usr':request.user})
-		return render(request, 'post.html', {'posts':post , 'x':"Like" , 'usr':request.user})
+			return render(request,'post.html',{'posts':post , 'x':"Unlike" , 'usr':request.user , 'y':"Logout" , 'comment':c})
+		return render(request, 'post.html', {'posts':post , 'x':"Like" , 'usr':request.user , 'y':"Logout", 'comment':c })
 	else:
-		return render(request,'post.html',{'posts':post , 'x':"Like" , 'usr':request.user})
+		return render(request,'post.html',{'posts':post , 'x':"Like" , 'usr':request.user , 'y':"Login" })
 
 def contact(request):
-	return render(request,'contact.html')
+	if request.user.is_authenticated():
+		return render(request,'contact.html',{'x':"Logout"})
+	else:
+		return render(request,'contact.html',{'x':"Login" })
 
 def create(request):
 	usr=request.GET.get('username')
@@ -70,7 +90,7 @@ def create(request):
 		user = User.objects.create_user(usr,email,pswd)
 		user.save()
 		data = Data.objects.all()
-		return render(request,'newfile.html',{ 'data':data})
+		return render(request,'newfile.html',{ 'data':data,})
 	else:
 		response="enter all details"
 
@@ -78,7 +98,10 @@ def create(request):
 
 def articles(request):
 	data=Data.objects.all()
-	return render(request,'articles.html',{ 'data':data })
+	if request.user.is_authenticated():
+		return render(request,'articles.html',{ 'data':data , 'x':"Logout"})
+	else : 
+		return render(request,'articles.html',{'data':data , 'x':"Login" })
 
 def change(request):
 	user = request.GET.get('username')
@@ -96,9 +119,11 @@ def change(request):
 def search(request):
 	query = request.GET.get('q')
 	if query:
-		flag = 1
 		data = Data.objects.filter(title__icontains = query)
-		return render(request,'list.html',{'data':data})
+		if request.user.is_authenticated():
+			return render(request,'list.html',{'data':data , 'x':"Logout" })
+		else:
+			return render(request,'list.html',{'data':data , 'x':"Login" })
 	else :
 		response = "Enter your query"
 
@@ -110,39 +135,23 @@ def change1(request):
 def reach(request):
 	return render(request,'list.html')
 
-def logout(request):
-	logout(request)
 
 def goback(request):
-	data = Data.objects.all()
-	return render(request,'newfile.html',{ 'data':data })
+	if request.user.is_authenticated():
+		data = Data.objects.all()
+		return render(request,'newfile.html',{'data':data , 'x':"Logout" })
+	else:
+		data = Data.objects.all()
+		return render(request,'newfile.html',{'data':data , 'x':"Login" })
 
-def forgot(request):
-	return render(request,'passwordreset.html')
 
-def suggest(request):
-	if request.method=='POST':
-		data = Data.objects.filter(title__icontains = query)
-		#return render(request,'list.html',{'data':data})
-	print "hihi"
-	ctx = {'data':data}
-	return HttpResponse(json.dumps(ctx) , content_type='application/json')
-
-# def like_post(request):
-# 	l = likes.objects.filter(user = request.user , post = post_title)
-# 	if not l:
-# 		post_title.number_likes += 1
-# 		like = likes.objects.create(user=request.user , post = post_title)
-# 		post_title.save()
-# 	else:
-# 		post_title.number_likes -= 1
-# 		like = likes.objectls.filter(user=request.user , post = post_title)
-# 		like.delete()
-# 		post_title.save()
-# 	return post(request, post_title.slug)
-
-def ret(request):
-	return render(request,'file.html')
+def go(request):
+	if request.user.is_authenticated():
+		data = Data.objects.all()
+		return render(request,'newfile.html',{'data':data , 'x':"Logout"})
+	else:
+		data = Data.objects.all()
+		return render(request,'newfile.html',{'data':data , 'x':"Login"})
 
 def like(request):
 	if request.method == 'POST':
@@ -165,4 +174,22 @@ def like(request):
 			return HttpResponse(json.dumps(ctx), content_type='application/json')
 		else :
 			return render(request,'file.html')
-		
+
+def ret(request):
+	return render(request,'file.html')
+
+def commenting(request):
+	if request.method == 'POST':
+		comm = request.POST.get("comment")
+		cmnt = comment.objects.filter(post = post_title)
+		#import pdb; pdb.set_trace()
+		if not comm == "":
+			obj = comment(user = request.user, post = post_title, text = comm)
+			obj.save()
+			post_title.number_comments += 1
+			post_title.save()
+		ctx = {'num_comm':post_title.number_comments , 'usr':unicode(request.user), 'tex':unicode(comm) ,  'date':obj.date_created.strftime('%b %d, %Y ')}
+		return HttpResponse(json.dumps(ctx), content_type='application/json')
+
+
+
