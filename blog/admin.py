92f1baa32846c805daedb6d9b4dfde888ee34bdd
Shalaka Sinha <Shalaka@Shalaka-MacBook-Air.local>
2016-01-26 10:30:08 +0530

    comment and like models registered

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Admin , Data
+from .models import Admin , Data , likes ,comment
 	
 # Register your models here.
 admin.site.register(Admin)
 admin.site.register(Data)
+admin.site.register(likes)
+admin.site.register(comment)
